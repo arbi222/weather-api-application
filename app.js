bb0cd7b5159b58a70a768dbcf2c1343dd072bdc2
Arbi <arbihamolli111@gmail.com>
2025-06-10 23:26:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ app.post("/" , function(req, res){
 });
 
 const backendUrl = "https://weatherlive-9kk4.onrender.com";
-const job = new cron.CronJob('*/14 * * * *', function(){
+const job = new cron.CronJob('*/10 * * * *', function(){
   https.get(backendUrl, (res) => {
     if (res.statusCode === 200){
       console.log("server restarted")
